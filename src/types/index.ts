2c54ac8326d5c502abb5d743db1815d1baa5b94f
Angleito <arainey555@gmail.com>
2025-05-01 21:57:44 -0700

    commit error

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,9 @@ export interface Config {
   network: string;
   walletAddress: string;
   encryptedStorage: boolean;
+  lastDeployment?: {
+    packageId: string;
+  };
 }
 
 export * from './todo';
