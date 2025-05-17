ab953ddc8560ef9831922e6f355b588620db485b
Angleito <arainey555@gmail.com>
2025-05-17 14:13:53 -0700

    Fixed CLI, and Updated TS/JS Code

--- src/__mocks__/@mysten/sui.js/index.ts
+++
@@ -1,6 +0,0 @@
-// Export the BCS implementation
-import { BCS, bcs } from './bcs';
-export { BCS, bcs };
-
-// Re-export other mock modules
-export * from './client';
--- src/services/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TodoService = exports.ConfigService = void 0;
-var config_service_1 = require("./config-service");
-Object.defineProperty(exports, "ConfigService", { enumerable: true, get: function () { return config_service_1.ConfigService; } });
-var todoService_1 = require("./todoService");
-Object.defineProperty(exports, "TodoService", { enumerable: true, get: function () { return todoService_1.TodoService; } });
